775ade46510035bd731849fb0a32571019975d43
davisjrr <dclimaj@outlook.com>
2026-08-31 22:22:35 -0300

    feat: criação da classe filha Aluno com construtor e método herdados da classe base

--- EscolaTecnica/Aluno.cs
+++ EscolaTecnica/Aluno.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EscolaTecnica
+﻿namespace EscolaTecnica
 {
-    internal class Aluno
+    class Aluno : Pessoa
     {
+        public string Curso { get; set; }
+
+        // Construtor com herança (base no C#, primo do super do Java)
+        public Aluno(string nome, string curso) : base(nome)
+        {
+            Curso = curso;
+        }
+
+        // Método pra sobreescrever o método herdado da classe base
+        public override void ExibirInfos()
+        {
+            Console.WriteLine($"ALUNO: {Nome} / CURSO: {Curso}");
+        }
     }
 }
